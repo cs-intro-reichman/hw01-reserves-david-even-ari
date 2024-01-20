d676611d1bfd13780225d589c015197680d13965
david <david.evenari@post.runi.ac.il>
2024-01-20 21:41:32 +0200

    finished addTwo file

--- AddTwo.java
+++ AddTwo.java
@@ -3,7 +3,9 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here
-		System.out.println("First Message!");
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
 	}
 }
